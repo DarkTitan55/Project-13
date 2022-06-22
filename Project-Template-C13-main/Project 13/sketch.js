af2665ae4f7f7b6737cf8d3e461cd2234c3c52f1
DarkTitan55 <103156986+DarkTitan55@users.noreply.github.com>
2022-06-22 15:08:39 -0700

    Add files via upload

--- Project-Template-C13-main/Project 13/sketch.js
+++ Project-Template-C13-main/Project 13/sketch.js
@@ -56,10 +56,10 @@ function draw() {
    
   //creating continous enemies
 
-  // var select_balloon = Math.round(random(1,4));
+  var select_balloon = Math.round(random(1,4));
   // var select_balloon = random(1,4);
   // var select_balloon = Math.round(random());
-   var select_balloon = Math.round(random(1,4,2));
+  // var select_balloon = Math.round(random(1,4,2));
   
   if (World.frameCount % 100 == 0) {
     if (select_balloon == 1) {
@@ -93,13 +93,13 @@ function draw() {
   arrow.scale = 0.3;
 }
 
-// function redBalloon() {
-//   var red = createSprite(0,50, 10, 10);
-//   red.addImage(red_balloonImage);
-//   red.velocityX = 3;
-//   red.lifetime = 150;
-//   red.scale = 0.1;
-// }
+ function redBalloon() {
+   var red = createSprite(0,50, 10, 10);
+   red.addImage(red_balloonImage);
+   red.velocityX = 3;
+  red.lifetime = 150;
+   red.scale = 0.1;
+ }
 
 // function redBalloon() {
 //   var red = createSprite(,Math.round(random(20, 370)),50, 10, 10);
@@ -117,13 +117,13 @@ function draw() {
 //   red.scale = 0.1;
 // }
 
- function redBalloon() {
-   var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
-   red.addImage(red_balloonImage);
-   red.velocityX = 3;
-  red.lifetime = 150;
-   red.scale = 0.1;
- }
+ //function redBalloon() {
+  // var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
+  // red.addImage(red_balloonImage);
+  // red.velocityX = 3;
+ // red.lifetime = 150;
+  // red.scale = 0.1;}
+
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
